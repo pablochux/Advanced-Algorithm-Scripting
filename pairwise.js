02a565ed09196285b70cf4b71e5a86b17b012ee9
pablochux15@gmail.com <pablochux15@gmail.com>
2016-09-14 12:10:42 +0200

    Add pairwise

--- pairwise.js
+++ pairwise.js
@@ -1,42 +1,26 @@
+// Find pair elements
 function pairwise(arr, arg) {
-    var counter = 0;
-    var total = arr.reduce(function(previousValue, currentValue, currentIndex) {
-        return (previousValue + currentValue === arg) ? currentIndex : 0;
-    }, 0);
-    // var newArr = arr.reduce(function(previousValue, currentValue, currentIndex) {
-    //     if (previousValue + currentValue) {
-    //         counter += currentIndex;
-    //         return true;
-    //     }
-    //     return false;
-    // });
-    console.log(total);
-    console.log(counter);
-    return arr;
+    // arr is an array and arg is a number
+    var keyCounter = 0,
+        keyUsedItems = [];
+    // Search every element in the array
+    for (var i = 0; i < arr.length; i++) {
+        // For each element, search for the pair
+        for (var j = 0; j < arr.length; j++) {
+            // Se comprueba que los indices no sean iguales (para que no se sumen los elementos), que ninguno de los elementos haya sido utilizado y que la suma de los elementos sea igual que arg
+            if ((i !== j) && (keyUsedItems.indexOf(i) === -1) && (keyUsedItems.indexOf(j) === -1) && (arr[i] + arr[j] === arg)) {
+                // Se añaden los elementos utilizados para que no se repitan
+                keyUsedItems.push(i, j);
+                keyCounter += j + i;
+            }
+        }
+    }
+    return keyCounter;
 }
-//
-// function pairwise(arr, arg) {
-//     // arr is an array and arg is a number
-//     var keyCounter = 0;
-//     // Search every element
-//     for (var i = 0; i < arr.length; i++) {
-//         // For each element, search for the pair
-//         for (var j = 0; j < arr.length; j++) {
-//             // No se sumen los mismos elementos
-//             if (i !== j) {
-//                 if (arr[i] + arr[j] === arg) {
-//                     keyCounter += j + i;
-//
-//                     // Problema -> duplicación de elementos
-//                     console.log('i: ' + i);
-//                     console.log('j: ' + j);
-//                     console.log(keyCounter);
-//                 }
-//             }
-//         }
-//     }
-//     console.log(keyCounter);
-//     return keyCounter / 2;
-// }
 
-pairwise([1, 4, 2, 3, 0, 5], 7); // 11 ((1 + 3) + (2 + 5))
+// Test cases
+pairwise([1, 4, 2, 3, 0, 5], 7); // should return 11.
+pairwise([1, 3, 2, 4], 4); // should return 1.
+pairwise([1, 1, 1], 2); // should return 1.
+pairwise([0, 0, 0, 0, 1, 1], 1); // should return 10.
+pairwise([], 100); // should return 0.
