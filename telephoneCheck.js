056cef182589e768f59346e1c9b5a8eecb16f296
pablochux15@gmail.com <pablochux15@gmail.com>
2016-09-16 19:32:56 +0200

    Add telephoneCheck

--- telephoneCheck.js
+++ telephoneCheck.js
@@ -1,24 +1,5 @@
 function telephoneCheck(str) {
-    var valid = false;
-    // numnumnum + algo + num num num + algo + num num num num
-    // Comprueba el caso 5555555555
-    valid = str.search(/[0-9]{3}[0-9]{3}[0-9]{4}/) !== -1;
-    // Comprueba el caso 555-555-5555
-    valid = str.search(/\-*[0-9]{3}\-*[0-9]{3}\-*[0-9]{4}/) !== -1;
-    console.log(str.search(/\-*[0-9]{3}\-*[0-9]{3}\-*[0-9]{4}/) !== -1);
-    // console.log(valid);
-    return valid;
+    var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+    return regex.test(str);
 }
-
-// Valid formats 3 numbers, 3 numbers and 4 numbers
-// 555-555-5555
-// (555)555-5555
-// (555) 555-5555
-// 555 555 5555
-// 5555555555
-// 1 555 555 5555
 telephoneCheck("555-555-5555");
-telephoneCheck("1 555-555-5555");
-telephoneCheck("1 (555) 555-5555");
-telephoneCheck("5555555555");
-telephoneCheck("(555)555-5555");
